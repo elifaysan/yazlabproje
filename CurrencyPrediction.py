5717afca8fa2b95cfbe629dab6a728f01bb19e58
Elif Aysan <elifaysan@hotmail.com>
2025-01-02 22:36:05 +0300

    initial commit

---
+++ CurrencyPrediction.py
@@ -0,0 +1,135 @@
+﻿import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.plotting import register_matplotlib_converters
+from tf_keras import Sequential
+from tf_keras.src.layers import LSTM, Dense, GRU, Bidirectional, Conv1D, MaxPooling1D, Flatten, GlobalAveragePooling1D
+
+sns.set_theme(style="darkgrid", font_scale=1.5)
+register_matplotlib_converters()
+df = pd.read_csv("usd_verileri.csv", header=None, names=["Date", "Price"], parse_dates=["Date"], dayfirst=True)
+df["Price"] = df["Price"].astype(str).str.replace(",", ".").astype(float)
+
+df.set_index("Date", inplace=True)
+
+df.resample('10D').mean().plot(figsize=(12,6))
+plt.figtext(.5,0.9,"Down-sampled to 10-day periods", fontsize=20, ha='center')
+plt.show()
+
+data = df.iloc[:, 0]
+
+hist = []
+target = []
+length = 90
+
+for i in range(len(data)-length):
+    x = data[i:i+length]
+    y = data[i+length]
+    hist.append(x)
+    target.append(y)
+
+print(len(hist[0]))
+print(len(hist))
+print(len(target))
+
+print(hist[0][length-1])
+print(data[length-1])
+
+print(hist[1][length-1])
+print(data[length])
+print(target[0])
+
+#convert list to array
+hist = np.array(hist)
+target = np.array(target)
+
+# Reverse the array
+hist = hist[::-1]
+target = target[::-1]
+
+target = target.reshape(-1,1)
+
+X_train = hist[:3500,:]
+X_test = hist[3500:,:]
+
+y_train = target[:3500]
+y_test = target[3500:]
+
+# LSTM Model
+# model = Sequential([
+#     LSTM(50, return_sequences= True, input_shape= (X_train.shape[1], 1)),
+#     LSTM(64, return_sequences= False),
+#     Dense(32),
+#     Dense(16),
+#     Dense(1)
+# ])
+
+# GRU Model
+# model = Sequential([
+#     GRU(50, return_sequences=True, input_shape=(X_train.shape[1], 1)),
+#     GRU(64, return_sequences=False),
+#     Dense(32, activation='relu'),
+#     Dense(16, activation='relu'),
+#     Dense(1)
+# ])
+
+# Bidirectional LSTM Model
+# model = Sequential([
+#     Bidirectional(LSTM(50, return_sequences=True), input_shape=(X_train.shape[1], 1)),
+#     Bidirectional(LSTM(64, return_sequences=False)),
+#     Dense(32, activation='relu'),
+#     Dense(16, activation='relu'),
+#     Dense(1)
+# ])
+
+# CNN-LSTM Hybrid Model (BEST)
+model = Sequential([
+    Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], 1)),
+    MaxPooling1D(pool_size=2),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1)
+])
+
+# Fully Convolutional Networks (FCN) Model
+# model = Sequential([
+#     Conv1D(filters=128, kernel_size=8, activation='relu', input_shape=(X_train.shape[1], 1)),
+#     Conv1D(filters=256, kernel_size=5, activation='relu'),
+#     Conv1D(filters=128, kernel_size=3, activation='relu'),
+#     GlobalAveragePooling1D(),
+#     Dense(50, activation='relu'),
+#     Dense(1)
+# ])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.summary()
+
+history = model.fit(X_train, y_train, epochs=30, batch_size=32)
+
+loss = history.history['loss']
+epoch_count = range(1, len(loss) + 1)
+plt.figure(figsize=(12,8))
+plt.plot(epoch_count, loss, 'r--')
+plt.legend(['Training Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show();
+
+# Prediction
+
+pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, pred)
+rmse = np.sqrt(mean_squared_error(y_test, pred))
+print(f"MAE: {mae}, RMSE: {rmse}, MSE: {history.history['loss'][-1]}, MAPE: {mae/np.mean(y_test)}, R-Squared: {1 - (np.sum((y_test - pred)**2) / np.sum((y_test - np.mean(y_test))**2))}")
+
+plt.figure(figsize=(12,8))
+plt.plot(y_test, color='blue', label='Real')
+plt.plot(pred, color='red', label='Prediction')
+plt.xlabel('Days')
+plt.ylabel('Price')
+plt.title('USD Price Prediction')
+plt.legend()
+plt.show()
---
+++ LSTMModel.py
@@ -0,0 +1,151 @@
+﻿import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tf_keras import Sequential
+from tf_keras.src.layers import LSTM, Dense
+
+print(tf.__version__)
+print(keras.__version__)
+
+# Veriyi yükleme
+file_path = "usd_verileri.csv"  # CSV dosyanızın yolu
+data = pd.read_csv(file_path, header=None, names=["Date", "Price"])
+
+# Döviz kurundaki virgülleri noktaya çevirme ve float türüne dönüştürme
+data["Price"] = data["Price"].astype(str).str.replace(",", ".").astype(float)
+# Tarih sütununu datetime formatına çevirme
+data["Date"] = pd.to_datetime(data["Date"], format="%d.%m.%Y")
+
+# Tarihi indeks olarak ayarlama
+data.set_index("Date", inplace=True)
+
+duplicate_dates = data.index[data.index.duplicated()]
+data = data[~data.index.duplicated(keep="first")]
+
+data.resample('10D').mean().plot(figsize=(12,6))
+
+# Veri görselleştirme
+plt.figure(figsize=(10, 6))
+plt.plot(data.index, data["Price"], label="USD/TRY Price", color="blue")
+plt.title("USD/TRY Price Over Time")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend()
+plt.grid()
+plt.show()
+
+n_cols = 1
+dataset = data["Price"]
+dataset = pd.DataFrame(dataset)
+data = dataset.values
+
+scaler = MinMaxScaler(feature_range= (0, 1))
+scaled_data = scaler.fit_transform(np.array(data))
+
+# 75% to Train , 25% to Test
+train_size = int(len(data) * 0.75)
+test_size = len(data) - train_size
+print("Train Size :",train_size,"Test Size :",test_size)
+
+# train_data = scaled_data[0:train_size, :]
+train_data = scaled_data[-train_size:, :]
+train_data.shape
+
+x_train = []
+y_train = []
+time_steps = 60
+n_cols = 1
+
+for i in range(time_steps, len(train_data)):
+    x_train.append(train_data[i-time_steps:i, :n_cols])
+    y_train.append(train_data[i, :n_cols])
+    if i<=time_steps:
+        print('X_train: ', x_train)
+        print('y_train:' , y_train)
+
+# Convert to numpy array
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = x_train[::-1]
+y_train = y_train[::-1]
+
+x_train.shape , y_train.shape
+
+model = Sequential([
+    LSTM(50, return_sequences= True, input_shape= (x_train.shape[1], n_cols)),
+    LSTM(64, return_sequences= False),
+    Dense(32),
+    Dense(16),
+    Dense(n_cols)
+])
+
+model.compile(optimizer= 'adam', loss= 'mse' , metrics= "mean_absolute_error")
+
+model.summary()
+
+# Fitting the LSTM to the Training set
+history = model.fit(x_train, y_train, epochs= 100, batch_size= 32)
+
+plt.figure(figsize=(12, 8))
+plt.plot(history.history["loss"])
+plt.plot(history.history["mean_absolute_error"])
+plt.legend(['Mean Squared Error','Mean Absolute Error'])
+plt.title("Losses")
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.show()
+
+print("MSE: ", history.history["loss"][-1])
+print("MAE: ", history.history["mean_absolute_error"][-1])
+
+# Creating a testing set with 60 time-steps and 1 output
+time_steps = 60
+test_data = scaled_data[train_size - time_steps: , :]
+
+x_test = []
+y_test = []
+n_cols = 1
+
+for i in range(time_steps, len(test_data)):
+    x_test.append(test_data[i-time_steps:i, 0:n_cols])
+    y_test.append(test_data[i, 0:n_cols])
+x_test, y_test = np.array(x_test), np.array(y_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], n_cols))
+
+x_test.shape , y_test.shape
+
+predictions = model.predict(x_test)
+
+#inverse predictions scaling
+predictions = scaler.inverse_transform(predictions)
+predictions.shape
+
+y_test = scaler.inverse_transform(y_test)
+
+RMSE = np.sqrt(np.mean( y_test - predictions )**2).round(2)
+print("RMSE: ", RMSE)
+
+preds_acts = pd.DataFrame(data={'Predictions':predictions.flatten(), 'Actuals':y_test.flatten()})
+preds_acts
+
+plt.figure(figsize = (16, 6))
+plt.plot(preds_acts['Predictions'])
+plt.plot(preds_acts['Actuals'])
+plt.legend(['Predictions', 'Actuals'])
+plt.show()
+
+train = dataset.iloc[:train_size , 0:1]
+test = dataset.iloc[train_size: , 0:1]
+test['Predictions'] = predictions
+
+plt.figure(figsize= (16, 6))
+plt.title('Prices Prediction', fontsize= 18)
+plt.xlabel('Date', fontsize= 18)
+plt.ylabel('Price', fontsize= 18)
+plt.plot(train['Price'], linewidth= 3)
+plt.plot(test['Price'], linewidth= 3)
+plt.plot(test["Predictions"], linewidth= 3)
+plt.legend(['Train', 'Test', 'Predictions'])
+plt.show()
